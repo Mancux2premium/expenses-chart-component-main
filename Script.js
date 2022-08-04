d204ece0f46c90713977a7a664271a5e401f202b
Mancux2 <mancux2@gmail.com>
2022-08-04 16:11:07 -0300

    se agrego el grafico.

---
+++ Script.js
@@ -0,0 +1,32 @@
+const labels = [
+    'mon',
+    'tue',
+    'wed',
+    'thu',
+    'fri',
+    'sat',
+    'sun'
+  ];
+
+  const data = {
+    labels: labels,
+    datasets: [{
+      label: 'dsdasd',
+      borderRadius:6,
+      backgroundColor: 'hsl(10, 79%, 65%)',
+      borderColor: 'hsl(33, 100%, 98%)',
+      data: [17.45, 34.91,52.36,31.07, 23.39, 43.28, 25.48],
+    }]
+  };
+
+  const config = {
+    type: 'bar',
+    data: data,
+    options: {}
+  };
+
+
+  const myChart = new Chart(
+    document.getElementById('myChart'),
+    config
+  );
