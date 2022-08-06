a5f4e2eba51154e27dcb5a700a59cf025b9f53eb
Mancux2 <mancux2@gmail.com>
2022-08-06 12:50:41 -0300

    json

--- Script.js
+++ Script.js
@@ -1,4 +1,4 @@
-const labels = [
+/* const labels = [
     'mon',
     'tue',
     'wed',
@@ -6,16 +6,53 @@ const labels = [
     'fri',
     'sat',
     'sun'
-  ];
+  ]; */
+const gastos =[
+  {
+    "day": "mon",
+    "amount": 17.45
+  },
+  {
+    "day": "tue",
+    "amount": 34.91
+  },
+  {
+    "day": "wed",
+    "amount": 52.36
+  },
+  {
+    "day": "thu",
+    "amount": 31.07
+  },
+  {
+    "day": "fri",
+    "amount": 23.39
+  },
+  {
+    "day": "sat",
+    "amount": 43.28
+  },
+  {
+    "day": "sun",
+    "amount": 25.48
+  }
+]
+const amount =[];
+const day = [];
+for(i=0;i<gastos.length;i++){
+  amount.push(gastos[i].amount)
+  day.push(gastos[i].day)
+}
+/* const jsonData = JSON.parse(gastos) */
 
   const data = {
-    labels: labels,
-    datasets: [{
-      label: 'dsdasd',
+     labels: day,
+      datasets: [{
+      label: 'Gastos diarios',
       borderRadius:6,
-      backgroundColor: 'hsl(10, 79%, 65%)',
+      backgroundColor: ['hsl(10, 79%, 65%)'],
       borderColor: 'none',
-      data: [17.45, 34.91,52.36,31.07, 23.39, 43.28, 25.48],
+      data: amount
     }]
   };
 
